32d052211ecef16141458773aa04e0a8d28d2031
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-06-01 13:08:14 -0400

    Refactored Text and related;

---
+++ octetmc-protocol/src/value/text/argb_colour.rs
@@ -0,0 +1,27 @@
+use super::*;
+
+
+/// A 32-bit ARGB colour for text.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub struct TextARGBColour {
+    /// Alpha lane.
+    pub a : u8,
+    /// Red lane.
+    pub r : u8,
+    /// Green lane.
+    pub g : u8,
+    /// Blue lane.
+    pub b : u8
+}
+
+impl Ser for TextARGBColour {
+    fn serialize<S>(&self, serer : S) -> Result<<S as Serer>::Ok, <S as Serer>::Error>
+    where
+        S : Serer
+    {
+        (((self.a as u32) << 24)
+        | ((self.r as u32) << 16)
+        | ((self.g as u32) << 8)
+        | (self.b as u32)).serialize(serer)
+    }
+}
---
+++ octetmc-protocol/src/value/text/content.rs
@@ -0,0 +1,53 @@
+use super::*;
+
+
+/// The content of a text component.
+#[derive(Clone, Debug, Ser)]
+#[serde(tag = "type")]
+pub enum TextContent<'l> {
+
+    /// Plain text.
+    #[serde(rename = "text")]
+    Literal {
+
+        /// The plain text to display directly.
+        #[serde(rename = "text")]
+        value : Cow<'l, str>
+
+    },
+
+    /// Translatable text.
+    ///
+    /// Translatable texts are resolved by the client based on their selected language.
+    #[serde(rename = "translatable")]
+    Translate {
+
+        /// The translation identifier, corresponding to the identifiers found in loaded language files.
+        #[serde(rename = "translate")]
+        key      : Cow<'l, str>,
+
+        /// If no corresponding translation can be found, this is used as the translated text.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        fallback : Option<Cow<'l, str>>,
+
+        /// A list of text components to be inserted into the string-interpolation slots in the translation text.
+        #[serde(skip_serializing_if = "slice_is_empty")]
+        with     : Cow<'l, [TextComponent<'l>]>
+
+    },
+
+    /// Keybind text.
+    ///
+    /// Keybind texts are resolved by the client based on their keybind settings.
+    #[serde(rename = "keybind")]
+    Keybind {
+
+        /// The keybind identifier.
+        ///
+        /// https://minecraft.wiki/w/Controls#Configurable_controls
+        #[serde(rename = "keybind")]
+        key : Cow<'l, str>
+
+    }
+
+}
